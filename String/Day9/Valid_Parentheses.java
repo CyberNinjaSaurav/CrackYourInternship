284c565cbfce393b392112a03c925ccf78ab7d1b
CyberNinjaSaurav <saurav2021ptu@gmail.com>
2024-07-31 23:26:48 +0530

    String

---
+++ String/Day9/Valid_Parentheses.java
@@ -0,0 +1,30 @@
+package CrackYourInternship.String.Day9;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+
+   
+        public boolean isValid(String s) {
+    
+            Stack<Character> stack = new Stack<Character>();
+    
+          
+    
+            for(char c : s.toCharArray()){
+                
+                if(c == '(')
+                stack.push(')');
+                else if( c== '{')
+                stack.push('}');
+    
+                else if( c == '[')
+                stack.push(']');
+    
+                else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+            }
+            return stack.isEmpty();
+    
+              }
+    }
